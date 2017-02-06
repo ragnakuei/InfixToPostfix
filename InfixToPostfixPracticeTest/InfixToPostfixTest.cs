f476f2a576169ae64f428215a96fc764b1903754
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-02-06 11:44:15 +0800

    2nd green light

--- InfixToPostfixPracticeTest/InfixToPostfixTest.cs
+++ InfixToPostfixPracticeTest/InfixToPostfixTest.cs
@@ -24,5 +24,23 @@ namespace InfixToPostfixPracticeTest
 
             expected.ToExpectedObject().ShouldEqual(actual);
         }
+
+        [TestMethod]
+        public void InfixToPostfix_1_add_2_multiple_3()
+        {
+            var input = "1+2*3";
+            var expected = new List<string> {
+                "1",
+                "2",
+                "3",
+                "*",
+                "+",
+            };
+            InfixToPostfix target = new InfixToPostfix(input);
+
+            var actual = target.Result;
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
     }
 }
